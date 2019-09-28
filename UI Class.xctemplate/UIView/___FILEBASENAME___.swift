6720867b74779c0ee49f64e8e74c76a25ec8fd14
devtak <hyotak.yun@gmail.com>
2019-09-28 12:04:18 +0900

    .

--- UI Class.xctemplate/UIView/___FILEBASENAME___.swift
+++ UI Class.xctemplate/UIView/___FILEBASENAME___.swift
@@ -3,12 +3,9 @@
 //  ___PROJECTNAME___
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
-//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
 //
 
 import UIKit
-import RxSwift
-import RxCocoa
 import CommonUtils
 
 final class ___FILEBASENAMEASIDENTIFIER___: UIView {
--- ViewModel.xctemplate/___FILEBASENAME___.swift
+++ ViewModel.xctemplate/___FILEBASENAME___.swift
@@ -3,13 +3,9 @@
 //  ___PROJECTNAME___
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
-//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
 //
 
 import Foundation
-import SwiftUI
-import Combine
-import CommonUtils
 
 final class ___FILEBASENAMEASIDENTIFIER___: ViewModel<___FILEBASENAMEASIDENTIFIER___.State, ___FILEBASENAMEASIDENTIFIER___.Action> {
     
